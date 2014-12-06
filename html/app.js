39b28674475635ac50c6ae3338ac6edb85deb188
nmonti <nickmax123@gmail.com>
2014-12-06 16:22:13 -0500

    skeleton stuff

--- html/app.js
+++ html/app.js
@@ -1,7 +1,45 @@
+var concentration
 $(document).ready(function() {
-  var concentration
-  $('#next').click(function() {
+  $('#gotoClassSelect').click(function() {
     concentration = $('#concentrations option:selected').text()
     $('.home').hide()
+    showClassSelect(concentration)
   })
+
+
 })
+
+function showClassSelect(concentration){
+  switch (concentration){
+  case "Undecided":
+    alert("hi")
+    break
+  case "Actuarial":
+
+    break
+  case "Applied Mathematics":
+
+    break
+  case "Mathematical Computing":
+
+    break
+  case "Pure Mathematics":
+
+    break
+  case "Statistics":
+
+    break
+  case "Teaching":
+
+    break
+  case "Design Your Own":
+
+    break
+
+  default:
+    alert("An error has occured!")
+    break
+
+  }
+
+}
